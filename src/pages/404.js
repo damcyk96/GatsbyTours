5c79645ab6447520aa65fac4045f4f5c9be95003
Damian Obajtek <damian.obajtek@gmail.com>
2020-12-15 20:15:45 +0100

    starter

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const error = () => {
+  return <div>hello from error</div>
+}
+
+export default 404
---
+++ src/pages/tour.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const tour = () => {
+  return <div>hello from tour</div>
+}
+
+export default tour
