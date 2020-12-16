31f10c476fec773d40733b9689c91fa8204d013a
Damian Obajtek <damian.obajtek@gmail.com>
2020-12-16 22:22:53 +0100

    banner & footer

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "../css/banner.module.css"
+
+export const Banner = ({ children, title, info }) => {
+  return (
+    <div className={styles.banner}>
+      <h1>{title}</h1>
+      <p>{info}</p>
+      {children}
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import { FaAdobe } from "react-icons/fa"
+import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
 
 export default () => (
   <Layout>
-    <h1> teskt</h1>
-    <FaAdobe />
+    <SimpleHero>
+      <Banner title="continue exploring" info="lorem ipsum dolor sit amet">
+        <Link className="btn-white" to="/tours">
+          explore tours
+        </Link>
+      </Banner>
+    </SimpleHero>
   </Layout>
 )
