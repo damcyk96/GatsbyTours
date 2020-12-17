4b0021b005f3164698ff121eea6ee265b6ce3143
Damian Obajtek <damian.obajtek@gmail.com>
2020-12-18 00:01:15 +0100

    fixes

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -2,22 +2,25 @@ import React from "react"
 import Title from "../Title"
 import styles from "../../css/about.module.css"
 import img from "../../images/defaultBcg.jpeg"
-
 const About = () => {
   return (
     <section className={styles.about}>
       <Title title="about" subtitle="us" />
       <div className={styles.aboutCenter}>
         <article className={styles.aboutImg}>
-          <img src={img} alt="about" />
+          <div className={styles.imgContainer}>
+            <img src={img} alt="about company" />
+          </div>
         </article>
         <article className={styles.aboutInfo}>
           <h4>explore the difference</h4>
           <p>
-            Laboris consequat eiusmod tempor nulla ut. Incididunt sit ea ea
-            officia in dolor reprehenderit amet. Labore proident consequat nulla
-            non consectetur eiusmod esse ex eiusmod cupidatat sunt ex. Cillum
-            incididunt consequat consectetur.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+            doloribus enim necessitatibus?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+            doloribus enim necessitatibus?
           </p>
           <button type="button" className="btn-primary">
             Read more
