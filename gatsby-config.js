db789188d1b5d5b538c4e923aa6074106b096f17
Damian Obajtek <damian.obajtek@gmail.com>
2020-12-19 13:18:49 +0100

    optimization for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+  siteMetadata: {
+    title: "BackRoads",
+    description: "explore awesome worldwide tours",
+    author: "@damcyk96",
+    data: ["item1", "item2"],
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,34 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
-import SimpleHero from "../components/SimpleHero"
 import Banner from "../components/Banner"
 import About from "../components/Home/About"
 import Services from "../components/Home/Services"
+import StyledHero from "../components/StyledHero"
+import { graphql } from "gatsby"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <SimpleHero>
+    <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
       <Banner title="continue exploring" info="lorem ipsum dolor sit amet">
         <Link className="btn-white" to="/tours">
           explore tours
         </Link>
       </Banner>
-    </SimpleHero>
+    </StyledHero>
     <About />
     <Services />
   </Layout>
 )
+
+export const query = graphql(`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4096) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`)
