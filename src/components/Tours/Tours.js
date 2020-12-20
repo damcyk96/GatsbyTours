2e5e3972dff4df6ed64367b6396b28f41e036539
Damian Obajtek <damian.obajtek@gmail.com>
2020-12-20 23:40:43 +0100

    tours

--- src/components/Tours/Tours.js
+++ src/components/Tours/Tours.js
@@ -1,7 +1,36 @@
 import React from "react"
+import TourList from "./TourList"
+import { useStaticQuery, graphql } from "gatsby"
+
+const getTours = graphql`
+  query {
+    tours: allContentfulTour {
+      edges {
+        node {
+          name
+          price
+          slug
+          country
+          contentful_id
+          days
+          images {
+            fluid {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const Tours = () => {
-  return <div></div>
+  const { tours } = useStaticQuery(getTours)
+  return (
+    <div>
+      <TourList tours={tours} />
+    </div>
+  )
 }
 
 export default Tours
