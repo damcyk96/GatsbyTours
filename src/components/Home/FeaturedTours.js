25d05ff7e0bfed0ae07e72742175efbce7e313f6
Damian Obajtek <damian.obajtek@gmail.com>
2020-12-20 22:03:16 +0100

    featured tours

--- src/components/Home/FeaturedTours.js
+++ src/components/Home/FeaturedTours.js
@@ -2,12 +2,12 @@ import React from "react"
 import Tour from "../Tours/Tour"
 import { useStaticQuery, graphql } from "gatsby"
 import styles from "../../css/items.module.css"
-import AniLink from "gatsby-plugin-transition-link"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Title from "../Title"
 
 const getTours = graphql`
   query {
-    featured: allContentfulTour(filter: { featured: { eq: true } }) {
+    featuredTours: allContentfulTour(filter: { featured: { eq: true } }) {
       edges {
         node {
           name
@@ -29,17 +29,14 @@ const getTours = graphql`
 
 const FeaturedTours = () => {
   const response = useStaticQuery(getTours)
-  const tours = response.featured.edges
+  const tours = response.featuredTours.edges
 
   return (
     <section className={styles.tours}>
       <Title title="featured" subtitle="tours" />
-      <div className={styles.center}>
-        {tours.map(({}) => {
-          return <Tour key={node.contentful_id} tour={node} />
-        })}
-      </div>
-      <Tour />
+      {tours.map(({ node }) => {
+        return <Tour key={node.contenful_id} tour={node} />
+      })}
       <AniLink fade to="/tours" className="btn-primary">
         All tours
       </AniLink>
--- src/components/Tours/Tour.js
+++ src/components/Tours/Tour.js
@@ -12,6 +12,22 @@ const Tour = ({ tour }) => {
     <article className={styles.tour}>
       <div className={styles.imgContainter}>
         <Image fluid={mainImage} className={styles.img} alt="single tour" />
+        <AniLink fade className={styles.link} to={`/tours/${slug}`}>
+          Details
+        </AniLink>
+      </div>
+      <div className={styles.footer}>
+        <h3>{name}</h3>
+        <div className={styles.info}>
+          <h4 className={styles.country}>
+            <FaMap className={styles.icon} />
+            {country}
+          </h4>
+          <div className={styles.details}>
+            <h6>{days}</h6>
+            <h6>{price}</h6>
+          </div>
+        </div>
       </div>
     </article>
   )
