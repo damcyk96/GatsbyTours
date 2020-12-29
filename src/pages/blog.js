a9f27822345314c2ea3cfae3882f7d2e9f75f6c4
Damian Obajtek <damian.obajtek@gmail.com>
2020-12-29 22:46:11 +0100

    blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
 import { graphql } from "gatsby"
-
+import BlogList from "../components/Blog/BlogList"
 const blog = ({ data }) => {
   return (
     <Layout>
       <StyledHero img={data.blogBcg.childImageSharp.fluid} />
+      <BlogList />
     </Layout>
   )
 }
-
-export default blog
 
 export const query = graphql`
   query {
@@ -24,3 +24,5 @@ export const query = graphql`
     }
   }
 `
+
+export default blog
